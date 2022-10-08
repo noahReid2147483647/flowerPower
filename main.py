8d930c0ede32d08ce99790e60586279ede4fa6b7
davidoffalexandra <103214944+davidoffalexandra@users.noreply.github.com>
2022-10-08 10:34:12 -0400

    UI it 1

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from tkinter import *
+from tkinter.filedialog import askopenfile
 
 root = Tk()
+root.title("Flower Identification")
+# create labels
 insertImageLbl = Label(root, text="Insert image here:")
 flowerDeterminedLbl = Label(root, text="The flower you inserted is: ")
 percentMatchLbl = Label(root, text="It is a x% match")
+# create button
 generateButton = Button(text="Generate Flower Name")
 
-#add frames for images?
+
+# add frames for images
+
+def upload_img():
+    file_path = askopenfile(mode='r', filetypes=[('Image Files', '*jpg', '*jpeg')])
+    if file_path is not None: pass
+
 
 insertImageLbl.grid(row=3, column=1)
 flowerDeterminedLbl.grid(row=2, column=4)
